13b768f11d9565c2c0c9fa8bfcec0d00a9aa835d
Deep <kronosdp3123@gmail.com>
2024-08-31 02:24:20 +0530

    Add test for viewing available books

--- tests/test_library.py
+++ tests/test_library.py
@@ -22,3 +22,14 @@ def test_return_book():
     library.borrow_book("123456789")
     library.return_book("123456789")
     assert len(library.available_books()) == 1
+    
+def test_view_available_books():
+    library = Library()
+    book1 = Book("123456789", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+    book2 = Book("987654321", "1984", "George Orwell",1949)
+    library.add_book(book1)
+    library.add_book(book2)
+    library.borrow_book("123456789")
+    available_books = library.available_books()
+    assert len(available_books) == 1
+    assert available_books[0].isbn == "987654321"
