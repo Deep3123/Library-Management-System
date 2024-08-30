d466f47584806cfa6c59dd140ede8e1accbb23b4
Deep <kronosdp3123@gmail.com>
2024-08-31 02:21:43 +0530

    Add test for returning books

--- tests/test_library.py
+++ tests/test_library.py
@@ -14,4 +14,11 @@ def test_borrow_book():
     library.add_book(book)
     library.borrow_book("123456789")
     assert len(library.available_books()) == 0
-
+    
+def test_return_book():
+    library = Library()
+    book = Book("123456789", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+    library.add_book(book)
+    library.borrow_book("123456789")
+    library.return_book("123456789")
+    assert len(library.available_books()) == 1
