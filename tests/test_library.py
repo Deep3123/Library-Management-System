21fa05e4de06b5e88b747d97f5933bc19d5f2737
Deep <kronosdp3123@gmail.com>
2024-08-31 02:15:01 +0530

    Add test for borrowing books

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,8 +1,17 @@
 import pytest
-from library import Library, Book
+from library_management_system.models import Book
+from library_management_system.library import Library
 
 def test_add_book():
     library = Library()
     book = Book("123456789", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
     library.add_book(book)
     assert len(library.available_books()) == 1
+    
+def test_borrow_book():
+    library = Library()
+    book = Book("123456789", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+    library.add_book(book)
+    library.borrow_book("123456789")
+    assert len(library.available_books()) == 0
+
